d30025253a5e8a38f8c0cd12fd38b5b63a53e3a7
RaimundBarbuta <reimiundbarbuta@gmail.com>
2018-02-16 13:32:42 +0000

    initial commit

---
+++ routes/urlapi.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var randomatic = require('randomatic');
+//#####Connect to mongo db######
+mongoose.connect('mongodb://cubu:mancarica@ds235388.mlab.com:35388/blogbd')
+//#####Create a schema for the db#####
+var Schema = mongoose.Schema;
+
+var url = new Schema({
+  urlID: String,
+  url: String,
+  shortUrl: String
+});
+
+var urlModel = mongoose.model('URL', url);
+
+//#####Routes Handlers#####
+//######## Gets the url ##########
+router.get('/', function(req, res, next) {
+  urlModel.findOne({urlID: req.query.urlID}, function(err, url){
+    if(err){
+      console.log(err);
+    }else{
+      console.log(url);
+      res.json(url);
+      console.log('GOT IT');
+    }
+  });
+});
+
+//#######Post a url#############
+router.post('/', function(req, res, next) {
+  var ids = randomatic('aA0', 8);
+  var urls  = new urlModel ({
+    urlID: ids,
+    url: req.body.url,
+    shortUrl: 'http://188.166.172.54/' + ids
+  });
+  urls.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.send('http://188.166.172.54/' + ids);
+    }
+  });
+});
+
+module.exports = router;
